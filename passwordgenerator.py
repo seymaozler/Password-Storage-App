10344978df1f7e086b25d93ef05ce210e9db2f47
seymaozler <seymaaozlerr@gmail.com>
2022-07-24 22:18:51 +0300

    Added search function

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -50,15 +50,43 @@ def save():
         messagebox.showinfo(
             title="oops", message="Please make sure you haven't left any fields empty")
     else:
-        with open("data1.json", "r") as data1_file:
-            # json dosyasını okuma
-            data1 = json.load(data1_file)
-            # updating old data with new data
-            data1.update(new_data)
-            # json dosyasına yazma
-            json.dump(new_data, data1_file, indent=4)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+# Find Password
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(
+                title="Error", message=f"No details for {website} exist.")
 
 
 window = Tk()
@@ -71,7 +99,7 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # labels
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
@@ -79,8 +107,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -88,6 +116,9 @@ email_entry.insert(0, "seymaaozlerr@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
+
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(
     text="Generate Password", command=password_generator)
 generate_password_button.grid(row=3, column=2)
